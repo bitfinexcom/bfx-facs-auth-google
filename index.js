8968783d48b8ccbce34b3ce8f3794954663784bc
face <face@myutil.com>
2018-07-15 03:41:44 -1000

    initial commit of GoogleAuth facility

---
+++ index.js
@@ -0,0 +1,168 @@
+'use strict'
+
+const _ = require('lodash')
+const Base = require('bfx-facs-base')
+const uuidv4 = require('uuid/v4')
+const {google} = require('googleapis')
+
+class GoogleAuth extends Base {
+  constructor (caller, opts, ctx) {
+    super(caller, opts, ctx)
+
+    this.name = 'auth-google'
+    this._hasConf = true
+
+    this.init()
+  }
+
+  //loginAdmin (params, ip, cb) {
+  //  const keys = _.keysIn(params)
+   // const googleLoginKeys = keys.includes('access_token') &&
+    //  keys.includes('token_type') &&
+     // keys.includes('expiry_date')
+
+    //if (googleLoginKeys) {
+      //this._loginAdminGoogle(params, ip, cb)
+    //} else {
+      //return cb(new Error('AUTH_FAC_LOGIN_KEYS_MISSING'))
+    //}
+  //}
+  // TODO: remove non google login
+  loginAdmin (params, ip, cb) {
+    const keys = _.keysIn(params)
+    const passLoginKeys = keys.includes('username') &&
+      keys.includes('password')
+    const googleLoginKeys = keys.includes('access_token') &&
+      keys.includes('token_type') &&
+      keys.includes('expiry_date')
+
+    if (passLoginKeys || googleLoginKeys) {
+      return (passLoginKeys)
+        ? this._loginAdminPass(params, ip, cb)
+        : this._loginAdminGoogle(params, ip, cb)
+    } else {
+      return cb(new Error('CORE_LOGIN_KEYS_MISSING'))
+    }
+  }
+
+  async _loginAdminGoogle (params, ip, cb) {
+    try {
+      const email = await this.googleEmailFromToken(params)
+      const valid = this._validAdminUserGoogleEmail(email)
+      return (valid)
+        ? this._createAdminToken(email, ip, cb)
+        : cb(new Error('AUTH_FAC_ONLY_BITFINEX_ACCOUNTS_ARE_ALLOW'))
+    } catch (e) {
+      cb(new Error('AUTH_FAC_INCORRECT_GOOGLE_TOKEN'))
+    }
+  }
+
+  async _createAdminToken (user, ip, cb) {
+    const username = user.replace('@bitfinex.com', '')
+    const token = 'ADM-' + uuidv4()
+    const expiresAt = new Date()
+    expiresAt.setHours(expiresAt.getHours() + 8)
+    const expiresAtSeconds = 28800
+    const query = { username, token, ip, expiresAt, expiresAtSeconds }
+    try {
+      await this._createUniqueAndExpireDbToken(query)
+      return cb(null, { username, token, expiresAt })
+    } catch (e) {
+      return cb(new Error('AUTH_FAC_ADMIN_TOKEN_CREATE_ERROR'))
+    }
+  }
+
+  _tokenKey (query) {
+    return `admtokens:${query.token}:${query.ip}`
+  }
+
+  _createUniqueAndExpireDbToken (query) {
+    const ctx = this.caller
+    if (ctx.dbMongo_m0) { // kyc-ui
+      const mc = ctx.dbMongo_m0.db
+      const collection = 'admTokens'
+      return new Promise((resolve, reject) => {
+        mc.collection(collection)
+          .insertOne(query, (err, res) => {
+            if (err) return reject(err)
+            resolve()
+          })
+      })
+    } else { // ethfinex-admin-ui
+      const key = this._tokenKey(query)
+      ctx.redis_gc0.cli_rw.multi([
+        ['set', key, query],
+        ['expire', key, query.expiresAtSeconds]
+      ])
+    }
+  }
+
+  _validAdminUserGoogleEmail (mail) {
+    return this._whiteListEmail(mail)
+  }
+
+  preAdminTokenCheck (authToken) {
+    return (authToken && authToken.length === 2)
+      ? authToken[0].startsWith('ADM')
+      : false
+  }
+
+  async checkAdminRedis (authToken) {
+    const ctx = this.ctx
+    const preCheck = this.preAdminTokenCheck(authToken)
+    if (!preCheck) return false
+    const token = authToken[0]
+    const ip = authToken[1].ip
+    const key = this._tokenKey({ token, ip })
+    const data = await ctx.redis_gc0.cli_rw.get(key)
+    return !!data && data.username && data.username.length > 0
+  }
+
+  _getOAuth2Client () {
+    const {clientId, clientSecret} = this.conf
+    return new google.auth.OAuth2(
+      clientId,
+      clientSecret
+    )
+  }
+
+  googleEmailFromToken (token) {
+    return new Promise(async (resolve, reject) => {
+      const oAuth2Client = await this._getOAuth2Client()
+      oAuth2Client.setCredentials(token)
+      const oauth2 = google.oauth2({version: 'v2', auth: oAuth2Client})
+      oauth2.userinfo.v2.me.get(
+        (err, data) => {
+          if (err) reject(new Error('AUTH_FAC_ERROR_ASK_EMAIL:' + err.toString()))
+          else resolve(data.data.email)
+        })
+    })
+  }
+
+  _whiteListEmail (email) {
+    const {WHITE_LIST_EMAILS} = this.conf
+    return WHITE_LIST_EMAILS.includes(email)
+  }
+
+  // TODO: Remove basicAuthcheck
+  _loginAdminPass (params, ip, cb) {
+    const valid = this.basicAuthAdmLogCheck(params.username, params.password)
+    return (valid)
+      ? this._createAdminToken(params.username, ip, cb)
+      : cb(new Error('KYC_LOGIN_INCORRECT_USERNAME_PASSWORD'))
+  }
+
+  basicAuthAdmLogCheck (email, password) {
+    const {BASIC_AUTH_ADM_USERS} = this.conf
+    let valid = false
+    _.forEach(BASIC_AUTH_ADM_USERS, (user) => {
+      if (user.email === email) {
+        valid = (user.password === password)
+        return false
+      }
+    })
+    return valid
+  }
+}
+
+module.exports = GoogleAuth
