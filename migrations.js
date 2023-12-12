0886e6866a2c1f28eb41d396b57406d30f2f2138
grantfayvor <fayvorharrison101@gmail.com>
2023-12-12 12:27:57 +0100

    check if columns exists before trying to create them

--- migrations.js
+++ migrations.js
@@ -4,16 +4,26 @@ const tableName = 'admin_users'
 
 const migrations = [
   (_this, cb) => {
-    _this.db.run(`
-      ALTER TABLE ${tableName}
-      ADD manageAdminsPrivilege TINYINTEGER
-    `, cb)
+    _this.db.all(`PRAGMA table_info(${tableName})`, [], (err, rows) => {
+      if (err) return cb(err)
+      if (rows?.find(row => row.name === 'manageAdminsPrivilege')) return cb()
+
+      _this.db.run(`
+        ALTER TABLE ${tableName}
+        ADD manageAdminsPrivilege TINYINTEGER
+      `, cb)
+    })
   },
   (_this, cb) => {
-    _this.db.run(`
-      ALTER TABLE ${tableName}
-      ADD forms TEXT
-    `, cb)
+    _this.db.all(`PRAGMA table_info(${tableName})`, [], (err, rows) => {
+      if (err) return cb(err)
+      if (rows?.find(row => row.name === 'forms')) return cb()
+
+      _this.db.run(`
+        ALTER TABLE ${tableName}
+        ADD forms TEXT
+      `, cb)
+    })
   }
 ]
 
