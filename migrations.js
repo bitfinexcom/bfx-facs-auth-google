ec5dbb9683c1a1dcf28d5a057fbd45dc11d71a32
José Rosendo <jose.sanchez@bitfinex.com>
2025-11-24 11:32:02 -0300

    Create and use object TABLES

--- migrations.js
+++ migrations.js
@@ -1,59 +1,61 @@
 'use strict'
 
-const tableName = 'admin_users'
+const TABLES = {
+  ADMIN_USERS: 'admin_users'
+}
 
 const migrations = [
   (_this, cb) => {
-    _this.db.all(`PRAGMA table_info(${tableName})`, [], (err, rows) => {
+    _this.db.all(`PRAGMA table_info(${TABLES.ADMIN_USERS})`, [], (err, rows) => {
       if (err) return cb(err)
       if (rows?.find(row => row.name === 'manageAdminsPrivilege')) return cb()
 
       _this.db.run(`
-        ALTER TABLE ${tableName}
+        ALTER TABLE ${TABLES.ADMIN_USERS}
         ADD manageAdminsPrivilege TINYINTEGER
       `, cb)
     })
   },
   (_this, cb) => {
-    _this.db.all(`PRAGMA table_info(${tableName})`, [], (err, rows) => {
+    _this.db.all(`PRAGMA table_info(${TABLES.ADMIN_USERS})`, [], (err, rows) => {
       if (err) return cb(err)
       if (rows?.find(row => row.name === 'forms')) return cb()
 
       _this.db.run(`
-        ALTER TABLE ${tableName}
+        ALTER TABLE ${TABLES.ADMIN_USERS}
         ADD forms TEXT
       `, cb)
     })
   },
   (_this, cb) => {
-    _this.db.all(`PRAGMA table_info(${tableName})`, [], (err, rows) => {
+    _this.db.all(`PRAGMA table_info(${TABLES.ADMIN_USERS})`, [], (err, rows) => {
       if (err) return cb(err)
       if (rows?.find(row => row.name === 'passwordResetSentAt')) return cb()
 
       _this.db.run(`
-        ALTER TABLE ${tableName}
+        ALTER TABLE ${TABLES.ADMIN_USERS}
         ADD passwordResetSentAt DATETIME
       `, cb)
     })
   },
   (_this, cb) => {
-    _this.db.all(`PRAGMA table_info(${tableName})`, [], (err, rows) => {
+    _this.db.all(`PRAGMA table_info(${TABLES.ADMIN_USERS})`, [], (err, rows) => {
       if (err) return cb(err)
       if (rows?.find(row => row.name === 'passwordResetToken')) return cb()
 
       _this.db.run(`
-        ALTER TABLE ${tableName}
+        ALTER TABLE ${TABLES.ADMIN_USERS}
         ADD passwordResetToken TEXT
       `, cb)
     })
   },
   (_this, cb) => {
-    _this.db.all(`PRAGMA table_info(${tableName})`, [], (err, rows) => {
+    _this.db.all(`PRAGMA table_info(${TABLES.ADMIN_USERS})`, [], (err, rows) => {
       if (err) return cb(err)
       if (rows?.find(row => row.name === 'fetchMotivationsPrivilege')) return cb()
 
       _this.db.run(`
-        ALTER TABLE ${tableName}
+        ALTER TABLE ${TABLES.ADMIN_USERS}
         ADD fetchMotivationsPrivilege BOOLEAN
       `, cb)
     })
